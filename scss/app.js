e5814261ce236c5d0d8600cf19c7a1386a80bf75
Gbaboh122 <73718883+Gbaboh122@users.noreply.github.com>
2021-04-15 03:42:43 +0300

    Now on the top row

--- scss/app.js
+++ scss/app.js
@@ -1,5 +1,6 @@
 window._ = require('lodash');
 import 'bootstrap';
+import '@fortawesome/fontawesome-free'
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
@@ -17,7 +18,8 @@ try {
 $(document).ready(function () {
     let data = {
       home_team: {
-        name: "NPA Python",
+        name: "Lakers",
+        logo: "home-logo",
         score: 50,
         players: [
           { number: 6, foul: 3 },
@@ -28,14 +30,15 @@ $(document).ready(function () {
           { number: 36, foul: 0 },
           { number: 5, foul: 0 },
           { number: 7, foul: 5 },
-          { number: 9, foul: 0 },
+          { number: 9, foul: 5 },
           { number: 11, foul: 3 },
-          { number: 15, foul: 1 },
+          { number: 15, foul: 5 },
           { number: 20, foul: 2 },
         ],
       },
       away_team: {
-        name: "Mighty Barolle",
+        name: "Worriors",
+        logo: "away-logo",
         score: 48,
         players: [
           { number: 4, foul: 3 },
@@ -58,7 +61,12 @@ $(document).ready(function () {
         let output = "";
 
         for(let i=0; i<foul; i++){
-            output += '<div class="col-2" id="player-foul-'+ (i + 1) +'"> X </div>';
+            output +=
+              '<div class="col-2" id="player-foul-' +
+              (i + 1) +
+              '">' +
+              '<i class="fas fa-bahai"></i>' +
+              "</div>";
         }
 
         return output;
@@ -66,17 +74,47 @@ $(document).ready(function () {
   
     let buildFouler = (fouler, index) => {
         let output = "";
-        output += '<div class="row my-2" id="fouler-'+ (index + 1) +'">'
+        output += '<div class="row my-3" id="fouler-'+ (index + 1) +'">'
             + '<div class="col-2 text-danger">'+ fouler.foul +'</div>'
             + buildFouls(fouler.foul)
-
         +'</div>'
         return output;
     }
 
+    let buildNumbers = (number, index) => {
+        let output = "";
+        output += '<div class="row my-3 text-center" id="player-number-'+ (index + 1) +'"><div class="col"><span>'+ number +'</span></div></div>';
+        return output;
+    }
+
     data.home_team.players.forEach((item, index) => {
-        let foulsContainer = $("#fouls");
+        let foulsContainer = $("#home-fouls");
         foulsContainer.append(buildFouler(item, index));
-        console.log(item, index);
+
+        let numbersContainer = $("#home-numbers");
+        numbersContainer.append(buildNumbers(item.number, index));
     });
+
+    data.away_team.players.forEach((item, index) => {
+        let foulsContainer = $("#away-fouls");
+        foulsContainer.append(buildFouler(item, index));
+
+        let numbersContainer = $("#away-numbers");
+        numbersContainer.append(buildNumbers(item.number, index));
+    });
+
+    let buildLogo = (logo) => {
+        return (
+          '<img class="team-logo w-100" src="images/' +
+            logo +
+          '.png"/>'
+        );
+    }
+
+    let homeLogo = $('#home-logo');
+    homeLogo.html(buildLogo(data.home_team.logo));
+
+    let awayLogo = $("#away-logo");
+    awayLogo.html(buildLogo(data.away_team.logo));
+
 });
